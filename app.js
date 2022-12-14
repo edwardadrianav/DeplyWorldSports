5241d76404db35939b9f83c35a082e650acddd83
edwardadrianav <edwardadrianav@hotmail.com>
2022-12-13 21:07:27 -0500

    instalación de cors

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 import express from 'express';
+import cors from 'cors';
 import { router } from './Router/router.js';
 
 //const router = express.Router();
 const app = express();
 
-app.use('/', router)
-app.use(express.json)
+app.use(cors());
+app.use(express.json());
+app.use('/', router);
 
 var PORT = process.env.PORT || 8000;
 app.listen( PORT, () => {
